316274b6effe198c25183965096b1d6467ebcf79
haingoc <phanhaingoc@gmail.com>
2020-09-16 18:33:18 +0200

    build not found ?

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,13 +106,12 @@ const IndexPage = () => {
       </Carousel>
 
 
-      <p>Welcome to your new Gatsby site.</p>
+      {/* <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        {/* <Image /> */}
       </div>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
     </Layout>
   )
 }
